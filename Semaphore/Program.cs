66aa1aebcbb742ef459d745589e7f5a02e4dc42d
Sergey Bednyakov <sbednyakov@gmail.com>
2022-04-21 23:57:46 +0300

    Semaphore update

---
+++ Semaphore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Semaphore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Semaphore light;
+            light = Semaphore.Yellow;
+            Console.WriteLine("Change the light to {0}", light);
+        }
+    }
+    enum Semaphore : int 
+    {
+        Red=100,
+        Yellow =200,
+        Green=300
+    }
+}
