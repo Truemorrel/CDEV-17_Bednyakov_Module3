92c80b2d2e72e6aeaaa3e150645dcf2b206837ab
Sergey Bednyakov <sbednyakov@gmail.com>
2022-04-27 13:16:52 +0300

    master: add arithmetic

---
+++ Arithmetic/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int counter = 10;
+        Console.WriteLine("Value : {0} Increment: {1}", counter, ++counter);
+    }
+}
