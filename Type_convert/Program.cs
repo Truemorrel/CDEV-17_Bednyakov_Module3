d81a543a0c33b077d4209a6d1379015676bb51f2
Sergey Bednyakov <sbednyakov@gmail.com>
2022-04-27 21:12:47 +0300

    master: add type convert sample

---
+++ Type_convert/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		int olddata = 6;
+		string data = olddata.ToString();
+
+		Console.WriteLine(data);
+
+		Console.WriteLine(olddata);
+
+		Console.ReadKey();
+	}
+}
