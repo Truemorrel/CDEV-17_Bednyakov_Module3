45d9a8bf540e2ab44ece483bd9f63d58e48b47a5
Sergey Bednyakov <sbednyakov@gmail.com>
2022-04-27 13:38:49 +0300

    master: add remainder

---
+++ Remainder/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        double result = 10 % 3;
+        Console.WriteLine("10 % 3 = {0}", result);
+        Console.ReadKey();
+    }
+}
