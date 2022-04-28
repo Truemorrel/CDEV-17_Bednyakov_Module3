11df7db96750c8a61ab0b20259404dc97ea8ccaf
Sergey Bednyakov <sbednyakov@gmail.com>
2022-04-28 23:02:36 +0300

    master: parse age from int to byte

--- Type_convert/Program.cs
+++ Type_convert/Program.cs
@@ -20,9 +20,16 @@ namespace Type_convert
 			Console.WriteLine("Your age is {0} ", age);
 			Console.ReadKey();
 			Console.Write("Enter your age again: ");
-			
+			age = 0;
 			bool iscorrect = int.TryParse(Console.ReadLine(), out age);
 			Console.WriteLine("Your age is {0} ", age);
+			Console.ReadKey();
+			age = 0;
+			Console.Write("Enter your name: ");
+			string name = Console.ReadLine();
+			Console.Write("Enter your age second time more again: ");
+			byte b_age = (byte)int.Parse(Console.ReadLine());
+			Console.WriteLine("Your name is {0} and age is {1} ", name, b_age);
 			Console.ReadKey();
 		}
 	}
